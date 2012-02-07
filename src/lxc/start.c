ostap/lxc

commit 49296e2ebfe7c5f9d6ebafbb54f5c5e56a0cc085

support proper container reboot
This patch looks for Daniel's kernel patch allowing the lxc monitor
to tell container reboot from shutdown based on the exit signal.  If
that patch is not there, utmp monitoring is used.  Otherwise, it only
looks for the signal.  Note that the 'conf->need_utmp_watch' is
technically not necessary, as there is no harm in watching the utmp
file.

Signed-off-by: Serge Hallyn <serge.hallyn@canonical.com>

src/lxc/start.c
@@ -306,9 +306,11 @@ int lxc_poll(const char *name, struct lxc_handler *handler)
 		goto out_mainloop_open;
 	}
 
-	if (lxc_utmp_mainloop_add(&descr, handler)) {
-		ERROR("failed to add utmp handler to mainloop");
-		goto out_mainloop_open;
+	if (handler->conf->need_utmp_watch) {
+		if (lxc_utmp_mainloop_add(&descr, handler)) {
+			ERROR("failed to add utmp handler to mainloop");
+			goto out_mainloop_open;
+		}
 	}
 
 	return lxc_mainloop(&descr);
@@ -405,6 +407,28 @@ void lxc_abort(const char *name, struct lxc_handler *handler)
 		kill(handler->pid, SIGKILL);
 }
 
+#include <sys/reboot.h>
+#include <linux/reboot.h>
+
+static int must_drop_cap_sys_boot(void)
+{
+	FILE *f = fopen("/proc/sys/kernel/ctrl-alt-del", "r");
+	int ret;
+	int v;
+
+	if (!f)
+		return 1;
+
+	ret = fscanf(f, "%d", &v);
+	fclose(f);
+	if (ret != 1)
+		return 1;
+	ret = reboot(v ? LINUX_REBOOT_CMD_CAD_ON : LINUX_REBOOT_CMD_CAD_OFF);
+	if (ret != -1)
+		return 1;
+	return 0;
+}
+
 static int do_start(void *data)
 {
 	struct lxc_handler *handler = data;
@@ -439,10 +463,14 @@ static int do_start(void *data)
 		goto out_warn_father;
 	}
 
-	if (prctl(PR_CAPBSET_DROP, CAP_SYS_BOOT, 0, 0, 0)) {
-		SYSERROR("failed to remove CAP_SYS_BOOT capability");
-		return -1;
-	}
+	if (must_drop_cap_sys_boot()) {
+		if (prctl(PR_CAPBSET_DROP, CAP_SYS_BOOT, 0, 0, 0)) {
+			SYSERROR("failed to remove CAP_SYS_BOOT capability");
+			return -1;
+		}
+		handler->conf->need_utmp_watch = 1;
+	} else
+		handler->conf->need_utmp_watch = 0;
 
 	close(handler->sigfd);
 
@@ -577,6 +605,24 @@ int __lxc_start(const char *name, struct lxc_conf *conf,
 	while (waitpid(handler->pid, &status, 0) < 0 && errno == EINTR)
 		continue;
 
+        if (!WIFSIGNALED(status)) {
+                printf("child process exited but was not signaled\n");
+                return -1;
+        }
+
+	switch(WTERMSIG(status)) {
+	case SIGINT: /* halt */
+		DEBUG("Container halting");
+		break;
+	case SIGHUP: /* reboot */
+		DEBUG("Container rebooting");
+		handler->conf->reboot = 1;
+		break;
+	default:
+		DEBUG("unknown exit status for init: %d\n", WTERMSIG(status));
+		break;
+        }
+
 	err =  lxc_error_set_and_log(handler->pid, status);
 out_fini:
 	LXC_TTY_DEL_HANDLER(SIGQUIT);
@@ -627,5 +673,6 @@ int lxc_start(const char *name, char *const argv[], struct lxc_conf *conf)
 	if (lxc_check_inherited(-1))
 		return -1;
 
+	conf->need_utmp_watch = 1;
 	return __lxc_start(name, conf, &start_ops, &start_arg);
 }

src/lxc/conf.h
@@ -203,6 +203,7 @@ struct lxc_conf {
 	int tty;
 	int pts;
 	int reboot;
+	int need_utmp_watch;
 	int personality;
 	struct utsname *utsname;
 	struct lxc_list cgroup;

commit 54033c6313921ac40dbacf77f53667a3a8adf683

Don't raise error if container didn't sys_reboot
Don't call it an error if a container exits without calling sys_reboot.
Particularly since that will almost always be the case with lxc-execute.
This fixes a regression introduced in commit
"49296e2ebfe7c5f9d6ebafbb54f5c5e56a0cc085: support proper container
reboot"

Signed-off-by: Serge Hallyn <serge.hallyn@canonical.com>

src/lxc/start.c
@@ -612,22 +612,25 @@ int __lxc_start(const char *name, struct lxc_conf *conf,
 	while (waitpid(handler->pid, &status, 0) < 0 && errno == EINTR)
 		continue;
 
-        if (!WIFSIGNALED(status)) {
-                printf("child process exited but was not signaled\n");
-                return -1;
-        }
-
-	switch(WTERMSIG(status)) {
-	case SIGINT: /* halt */
-		DEBUG("Container halting");
-		break;
-	case SIGHUP: /* reboot */
-		DEBUG("Container rebooting");
-		handler->conf->reboot = 1;
-		break;
-	default:
-		DEBUG("unknown exit status for init: %d\n", WTERMSIG(status));
-		break;
+	/*
+	 * If the child process exited but was not signaled,
+	 * it didn't call reboot.  This should mean it was an
+	 * lxc-execute which simply exited.  In any case, treat
+	 * it as a 'halt'
+	 */
+        if (WIFSIGNALED(status)) {
+		switch(WTERMSIG(status)) {
+		case SIGINT: /* halt */
+			DEBUG("Container halting");
+			break;
+		case SIGHUP: /* reboot */
+			DEBUG("Container rebooting");
+			handler->conf->reboot = 1;
+			break;
+		default:
+			DEBUG("unknown exit status for init: %d\n", WTERMSIG(status));
+			break;
+		}
         }
 
 	err =  lxc_error_set_and_log(handler->pid, status);